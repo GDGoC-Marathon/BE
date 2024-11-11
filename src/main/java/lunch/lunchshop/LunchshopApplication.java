e52d369472a3b2c521b18d17b0fd4b7a7d427cba
blueskygree <133464320+blueskygree@users.noreply.github.com>
2024-11-11 20:58:38 +0900

    studentlunch crawling

---
+++ src/main/java/lunch/lunchshop/LunchshopApplication.java
@@ -0,0 +1,13 @@
+package lunch.lunchshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lunch/lunchshop/LunchshopApplicationTests.java
@@ -0,0 +1,13 @@
+package lunch.lunchshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
