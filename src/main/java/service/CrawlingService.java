a0fe1fb5b45a989b59aeda6848a3a161a9b4b847
daeyeon <kdy4410121@naver.com>
2024-11-12 19:44:08 +0900

    feat: url 지정 후 get으로 html 받아오는 함수 작성

---
+++ src/main/java/service/CrawlingService.java
@@ -0,0 +1,26 @@
+package service;
+
+import domain.Restaurant;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CrawlingService {
+
+    public List<Restaurant> getMenu() {
+    }
+
+    public String getHtmlFromUrl(String url){
+        RestTemplate restTemplate = new RestTemplate();
+        String html = restTemplate.getForObject(url, String.class);
+        return html;
+    }
+}
