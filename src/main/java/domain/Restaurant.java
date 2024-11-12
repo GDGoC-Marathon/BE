bbcf95c862aeca04951bcda024980f984431cecd
daeyeon <kdy4410121@naver.com>
2024-11-12 20:15:40 +0900

    feat: restaurant 도메인 생성

---
+++ src/main/java/domain/Restaurant.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Restaurant {
+
+    private String lunchName;
+    private String lunchItems;
+    private String price;
+
+    public Restaurant(String lunchName, String lunchItems, String price){
+        this.lunchName = lunchName;
+        this.lunchItems = lunchItems;
+        this.price = price;
+    }
+}
