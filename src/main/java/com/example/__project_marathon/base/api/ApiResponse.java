3e2308e4037bcab08ce482cf074aca380e22db57
whqtker <chosh2001@naver.com>
2024-11-12 15:53:34 +0900

    feat: ApiResponse 추가
    구현X

---
+++ src/main/java/com/example/__project_marathon/base/api/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.example.__project_marathon.base.api;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ApiResponse<T> {
+}
