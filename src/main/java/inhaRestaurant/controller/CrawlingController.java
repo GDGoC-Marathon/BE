e8779558bd6b32582c797dc59965a9a0c53448fd
daeyeon <kdy4410121@naver.com>
2024-11-14 16:00:54 +0900

    feat: 로그 생성

---
+++ src/main/java/inhaRestaurant/controller/CrawlingController.java
@@ -0,0 +1,42 @@
+package inhaRestaurant.controller;
+
+import inhaRestaurant.domain.Restaurant;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import inhaRestaurant.service.CrawlingService;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+public class CrawlingController {
+
+    private final String tableTag = ".table_1 tbody tr";
+    private final String thTag = "th[scope=row]";
+    private final String tdTag = "td.left";
+    private final String tdLast = "td";
+
+    @Autowired
+    private CrawlingService crawlingService;
+
+    @GetMapping("/restaurant")
+    public String getRestaurant(Model model){
+        String url = "https://www.inha.ac.kr/kr/1073/subview.do";
+        Document htmlInha = crawlingService.getHtmlFromUrl(url);
+        Elements elements = crawlingService.selectFromHtml(htmlInha, tableTag);
+        List<Restaurant> restaurants = crawlingService.addRestaurant(elements, thTag, tdTag, tdLast);
+        for (Restaurant restaurant : restaurants) {
+            log.info("restaurant 내용: " + restaurant.getLunchName());
+            log.info("restaurant 내용: " + restaurant.getLunchItems());
+            log.info("restaurant 내용: " + restaurant.getPrice());
+        }
+        model.addAttribute("restaurants", restaurants);
+        return "professorRestaurant";
+    }
+}
