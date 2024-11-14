ed420eaa0c423ba9cd977cb1d0878975730fb1cb
blueskygree <133464320+blueskygree@users.noreply.github.com>
2024-11-14 13:17:46 +0900

    studentlunch crawling

--- src/main/java/lunch/lunchshop/service/MenuService.java
+++ src/main/java/lunch/lunchshop/service/MenuService.java
@@ -14,12 +14,22 @@ public class MenuService {
         String url = "https://www.inha.ac.kr/kr/1072/subview.do";
         Document doc = Jsoup.connect(url).get();
 
+        // 주간 날짜 범위를 가져오기
+        String weekRange = doc.select("div.moveWeekBox.box_2 strong").text();
+
+        //날짜와 시간 가져오기
+        String date = doc.select("h2.objHeading_h2").first().text(); // "월요일 (11.11.)"
+        String time = doc.select("h3.objHeading_h3:contains(중식)").first().text();
+
         // Find the lunch section based on the specific header text for lunch (중식)
         Element lunchSection = doc.select("h3:contains(중식)").first().parent();
         Element table = lunchSection.select("div.table_1 table").first();
         Elements rows = table.select("tr");
 
         StringBuilder menuHtml = new StringBuilder();
+        menuHtml.append("<h2>주간 메뉴: ").append(weekRange).append("</h2>"); //주간범위
+        menuHtml.append("<h2>").append(date).append("</h2>");
+        menuHtml.append("<h3>").append(time).append("</h3>");
 
         for (Element row : rows) {
             String category = row.select("th").text();
