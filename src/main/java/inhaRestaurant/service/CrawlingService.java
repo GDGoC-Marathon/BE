7b3f221b235ae0f9502a643f570056598361067b
daeyeon <kdy4410121@naver.com>
2024-11-14 16:01:15 +0900

    feat: 로그 생성 및 코드 변경

--- src/main/java/inhaRestaurant/service/CrawlingService.java
+++ src/main/java/inhaRestaurant/service/CrawlingService.java
@@ -1,31 +1,37 @@
-package service;
+package inhaRestaurant.service;
 
-import domain.Restaurant;
+import inhaRestaurant.domain.Restaurant;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.awt.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class CrawlingService {
 
     private static final String nextLineHtml = "<br>";
     private static final String nextLineCode = "\n";
 
-    public String getHtmlFromUrl(String url){
-        RestTemplate restTemplate = new RestTemplate();
-        String html = restTemplate.getForObject(url, String.class);
-        return html;
+    public Document getHtmlFromUrl(String url) {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            log.info("document fetched successfully : " + doc.title());
+            return doc;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public Elements selectFromHtml(String html, String selectRows){
-        Document doc = Jsoup.parse(html);
+    public Elements selectFromHtml(Document doc, String selectRows){
         Elements rows = doc.select(selectRows);
         return rows;
     }
@@ -34,7 +40,11 @@ public class CrawlingService {
         List<Restaurant> restaurants = new ArrayList<>();
 
         for(Element row : rows){
+            log.info("Element 내용: " + row.text());  // 각 element의 텍스트만 출력
+            log.info("Element HTML: " + row.html());
             String lunchName = getTextFromHtml(row, selectTag1);
+
+            log.info(lunchName);
             String lunchItems = getHtmlFromHtml(row, selectTag2);
             String price = getTextFromLastHtml(row, selectTag3);
 
@@ -53,6 +63,6 @@ public class CrawlingService {
     }
 
     private String getTextFromLastHtml(Element row, String text){
-        return row.select(text).text();
+        return row.select(text).last().text();
     }
 }
