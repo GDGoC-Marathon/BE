67aba2ad90a34338f8c397b15ae3b8ffe3dbf0bc
daeyeon <kdy4410121@naver.com>
2024-11-15 13:47:21 +0900

    fix: 함수 제어자 변경

--- src/main/java/inhaRestaurant/Comment/service/CommentService.java
+++ src/main/java/inhaRestaurant/Comment/service/CommentService.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class CommentService {
 
     @Autowired
-    CommentRepository commentRepository;
+    private CommentRepository commentRepository;
 
     @Transactional
     public CommentDto createComment(CommentDto commentDto){
