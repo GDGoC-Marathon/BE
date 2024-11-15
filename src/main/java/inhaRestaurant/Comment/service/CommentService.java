def5f8544fd62f588617ff4bf8f90d04b4f87365
daeyeon <kdy4410121@naver.com>
2024-11-15 13:39:42 +0900

    feat: 댓글 작성 함수 생성

--- src/main/java/inhaRestaurant/Comment/service/CommentService.java
+++ src/main/java/inhaRestaurant/Comment/service/CommentService.java
@@ -1,5 +1,7 @@
 package inhaRestaurant.Comment.service;
 
+import inhaRestaurant.Comment.domain.Comment;
+import inhaRestaurant.Comment.domain.CommentDto;
 import inhaRestaurant.Comment.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,10 @@ public class CommentService {
     @Autowired
     CommentRepository commentRepository;
 
+    public CommentDto createComment(CommentDto commentDto){
+        Comment comment = Comment.createComment(commentDto);
+        Comment createdComment = commentRepository.save(comment);
+
+        return CommentDto.createCommentDto(createdComment);
+    }
 }
