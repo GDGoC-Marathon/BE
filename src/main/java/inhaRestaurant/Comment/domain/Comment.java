f2ea1d8991fe3918c01c0128433d28cfacde9c5f
daeyeon <kdy4410121@naver.com>
2024-11-15 13:37:46 +0900

    feat: 댓글 dto -> entity 함수 생성

--- src/main/java/inhaRestaurant/Comment/domain/Comment.java
+++ src/main/java/inhaRestaurant/Comment/domain/Comment.java
@@ -1,10 +1,12 @@
 package inhaRestaurant.Comment.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "inhaRestaurantComment")
 @NoArgsConstructor
+@Getter
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,10 +16,20 @@ public class Comment {
     @Column
     private String content;
 
-    public Comment(String id, String writerName, String content){
-        this.id = id;
+    public Comment(String writerName, String content){
         this.writerName = writerName;
         this.content = content;
+    }
+
+    public Comment createComment(CommentDto commentDto){
+        if(commentDto.getId() != null){
+            throw new IllegalArgumentException("dto 존재하지 않습니다.");
+        }
+
+        return new Comment(
+                commentDto.getWriterName(),
+                commentDto.getContent()
+        );
     }
 
 }
