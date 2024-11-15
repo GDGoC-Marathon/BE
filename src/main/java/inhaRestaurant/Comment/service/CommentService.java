75b782a8f9b5a2fd355dd67703a5534f799a8428
daeyeon <kdy4410121@naver.com>
2024-11-15 13:07:38 +0900

    feat: repository 의존성 주입 후 service 생성

---
+++ src/main/java/inhaRestaurant/Comment/service/CommentService.java
@@ -0,0 +1,13 @@
+package inhaRestaurant.Comment.service;
+
+import inhaRestaurant.Comment.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+}
