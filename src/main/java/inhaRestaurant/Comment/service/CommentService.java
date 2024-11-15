e1dbcff2bc5abc7f3e36f20fb8a77a4a6d5f0102
daeyeon <kdy4410121@naver.com>
2024-11-15 13:42:40 +0900

    feat: 댓글 조회 함수 생성

--- src/main/java/inhaRestaurant/Comment/service/CommentService.java
+++ src/main/java/inhaRestaurant/Comment/service/CommentService.java
@@ -3,8 +3,12 @@ package inhaRestaurant.Comment.service;
 import inhaRestaurant.Comment.domain.Comment;
 import inhaRestaurant.Comment.domain.CommentDto;
 import inhaRestaurant.Comment.repository.CommentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -12,10 +16,20 @@ public class CommentService {
     @Autowired
     CommentRepository commentRepository;
 
+    @Transactional
     public CommentDto createComment(CommentDto commentDto){
         Comment comment = Comment.createComment(commentDto);
         Comment createdComment = commentRepository.save(comment);
 
         return CommentDto.createCommentDto(createdComment);
     }
+
+    @Transactional
+    public List<CommentDto> comments(){
+        return commentRepository.finaAll()
+                .stream()
+                .map(CommentDto::createCommentDto)
+                .collect(Collectors.toList());
+    }
+
 }
