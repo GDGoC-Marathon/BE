5a721a2f50991739d284f5cf8287215ed688cf43
daeyeon <kdy4410121@naver.com>
2024-11-15 11:33:26 +0900

    feat: restaurant에 날짜 필드 추가

--- src/main/java/inhaRestaurant/domain/Restaurant.java
+++ src/main/java/inhaRestaurant/domain/Restaurant.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 @Getter
 public class Restaurant {
 
+    private String dayOfTheWeek;
     private String lunchName;
     private String lunchItems;
     private String price;
 
-    public Restaurant(String lunchName, String lunchItems, String price){
+    public Restaurant(String dayOfTheWeek, String lunchName, String lunchItems, String price){
+        this.dayOfTheWeek = dayOfTheWeek;
         this.lunchName = lunchName;
         this.lunchItems = lunchItems;
         this.price = price;
