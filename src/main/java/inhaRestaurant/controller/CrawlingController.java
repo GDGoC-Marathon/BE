3699fa1aa301b9aad66b55f6af647486d35f20f5
daeyeon <kdy4410121@naver.com>
2024-11-15 12:43:17 +0900

    fix: 공백 추가

--- src/main/java/inhaRestaurant/controller/CrawlingController.java
+++ src/main/java/inhaRestaurant/controller/CrawlingController.java
@@ -34,4 +34,5 @@ public class CrawlingController {
         model.addAttribute("restaurants", restaurants);
         return "professorRestaurant";
     }
+
 }
--- src/main/java/inhaRestaurant/domain/Restaurant.java
+++ src/main/java/inhaRestaurant/domain/Restaurant.java
@@ -16,4 +16,5 @@ public class Restaurant {
         this.lunchItems = lunchItems;
         this.price = price;
     }
+
 }
