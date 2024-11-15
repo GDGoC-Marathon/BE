6ba46889423023da05dfbf09da458e0db4ecf955
daeyeon <kdy4410121@naver.com>
2024-11-15 13:47:46 +0900

    feat: 댓글 컨트룰러 생성

---
+++ src/main/java/inhaRestaurant/Comment/controller/CommentController.java
@@ -0,0 +1,13 @@
+package inhaRestaurant.Comment.controller;
+
+import inhaRestaurant.Comment.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+}
