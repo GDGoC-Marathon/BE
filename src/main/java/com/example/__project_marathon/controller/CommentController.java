bbe98697ee31758b079c49ffe5b1646271351813
daeyeon <kdy4410121@naver.com>
2024-11-17 15:56:34 +0900

    feat: 내용에 따라 댓글 출력하는 함수 작성

--- src/main/java/com/example/__project_marathon/controller/CommentController.java
+++ src/main/java/com/example/__project_marathon/controller/CommentController.java
@@ -1,34 +1,38 @@
 package com.example.__project_marathon.controller;
 
 import com.example.__project_marathon.dto.CommentDto;
+import com.example.__project_marathon.model.Menu;
 import com.example.__project_marathon.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class CommentController {
 
-//    @Autowired
-//    private CommentService commentService;
-//
-//    @GetMapping("/api/restaurant/comment")
-//    public ResponseEntity<List<CommentDto>> comments(){
-//        List<CommentDto> commentDtos = commentService.comments();
-//
-//        return ResponseEntity.status(HttpStatus.OK).body(commentDtos);
-//    }
-//
-//    @PostMapping("/api/restaurant/comment")
-//    public ResponseEntity<CommentDto> createComment(@RequestBody CommentDto commentBody){
-//        CommentDto comment = commentService.createComment(commentBody);
-//        return ResponseEntity.status(HttpStatus.OK).body(comment);
-//    }
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/api/restaurant/comment")
+    public ResponseEntity<List<CommentDto>> comments(){
+        List<CommentDto> commentDtos = commentService.comments();
+
+        return ResponseEntity.status(HttpStatus.OK).body(commentDtos);
+    }
+
+    @PostMapping("/api/restaurant/comment")
+    public ResponseEntity<CommentDto> createComment(@RequestBody CommentDto commentBody){
+        CommentDto comment = commentService.createComment(commentBody);
+        return ResponseEntity.status(HttpStatus.OK).body(comment);
+    }
+
+    @GetMapping("api/restaurant/comment/{content}")
+    public ResponseEntity<List<CommentDto>> filterCommentByContent(@PathVariable String content){
+        List<CommentDto> commentDtos = commentService.searchContent(content);
+        return ResponseEntity.status(HttpStatus.OK).body(commentDtos);
+    }
 
 }
