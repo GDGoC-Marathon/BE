9018010c9153d0cb92f9df65070e1e3dc796b65f
daeyeon <kdy4410121@naver.com>
2024-11-17 15:56:52 +0900

    feat: 생성자 및 dto로부터 생성함수 추가

--- src/main/java/com/example/__project_marathon/model/Comment.java
+++ src/main/java/com/example/__project_marathon/model/Comment.java
@@ -1,5 +1,6 @@
 package com.example.__project_marathon.model;
 
+import com.example.__project_marathon.dto.CommentDto;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -11,6 +12,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,7 +23,17 @@ public class Comment extends BaseEntity {
     @Column
     private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "menu_id", nullable = false)
-    private Menu menu;
+    public Comment(String writer, String content){
+        this.writer = writer;
+        this.content = content;
+    }
+
+    public static Comment createComment(CommentDto commentDto){
+        if(commentDto.getId() != null){
+            throw new IllegalArgumentException("이미 존재하는 댓글입니다.");
+        }
+
+        return new Comment(commentDto.getWriter(), commentDto.getContent());
+    }
+
 }
