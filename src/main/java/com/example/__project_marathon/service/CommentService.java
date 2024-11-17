c900cbf581ab15fc79e5f4f01b9518ea9ad55675
daeyeon <kdy4410121@naver.com>
2024-11-17 15:53:16 +0900

    feat: 서비스에 함수 추가

--- src/main/java/com/example/__project_marathon/service/CommentService.java
+++ src/main/java/com/example/__project_marathon/service/CommentService.java
@@ -1,35 +1,44 @@
 package com.example.__project_marathon.service;
 
 import com.example.__project_marathon.dto.CommentDto;
+import com.example.__project_marathon.model.Menu;
 import com.example.__project_marathon.repository.CommentRepository;
 import com.example.__project_marathon.model.Comment;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
 
-//    @Autowired
-//    private CommentRepository commentRepository;
-//
-//    @Transactional
-//    public CommentDto createComment(CommentDto commentDto){
-//        Comment comment = Comment.createComment(commentDto);
-//        Comment createdComment = commentRepository.save(comment);
-//
-//        return CommentDto.createCommentDto(createdComment);
-//    }
-//
-//    @Transactional
-//    public List<CommentDto> comments(){
-//        return commentRepository.findAll()
-//                .stream()
-//                .map(CommentDto::createCommentDto)
-//                .collect(Collectors.toList());
-//    }
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Transactional
+    public CommentDto createComment(CommentDto commentDto){
+        Comment comment = Comment.createComment(commentDto);
+        Comment createdComment = commentRepository.save(comment);
+
+        return CommentDto.createCommentDto(createdComment);
+    }
+
+    @Transactional
+    public List<CommentDto> comments(){
+        return commentRepository.findAll()
+                .stream()
+                .map(CommentDto::createCommentDto)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<CommentDto> searchContent(String content){
+        return commentRepository.findByCommentContent(content).stream()
+                .map(CommentDto::createCommentDto)
+                .collect(Collectors.toList());
+    }
 
 }
