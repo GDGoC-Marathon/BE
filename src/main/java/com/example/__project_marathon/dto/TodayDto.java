3cb09c34d9ce5af4fc59e93e35b31c487a01c7cf
whqtker <chosh2001@naver.com>
2024-11-17 11:48:57 +0900

    feat: TodayDto 추가

---
+++ src/main/java/com/example/__project_marathon/dto/TodayDto.java
@@ -0,0 +1,14 @@
+package com.example.__project_marathon.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class TodayDto {
+    private Long id;
+    private Integer count;
+    private String img_url;
+}
