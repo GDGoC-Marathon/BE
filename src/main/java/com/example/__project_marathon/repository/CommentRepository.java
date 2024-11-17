b1351859f6d31c1339b92e0f7759a282c372ba50
daeyeon <kdy4410121@naver.com>
2024-11-17 15:49:26 +0900

    feat: 댓글 검색 함수 추가

--- src/main/java/com/example/__project_marathon/repository/CommentRepository.java
+++ src/main/java/com/example/__project_marathon/repository/CommentRepository.java
@@ -2,13 +2,17 @@ package com.example.__project_marathon.repository;
 
 import com.example.__project_marathon.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, String> {
 
     ArrayList<Comment> findAll();
+    @Query(value = "SELECT * FROM Comment WHERE content Like %?1%", nativeQuery = true)
+    List<Comment> findByStoreId(String content);
 
 }
